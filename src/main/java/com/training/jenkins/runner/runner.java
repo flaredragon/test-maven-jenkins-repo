03ba8cce17051e26986cc6570e54bb41f0a0c6b2
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2020-09-08 08:09:55 +0530

    Initial Commit

---
+++ src/main/java/com/training/jenkins/runner/runner.java
@@ -0,0 +1,15 @@
+package com.training.jenkins.runner;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ans = 1;
+		for(int i=0;i<=10;i++) {
+			if(i>0)
+				ans = ans * i;
+			System.out.println("Factorial of "+ i + " is " + ans+ "\n");
+		}
+	}
+
+}
